35ebf37e096a0e81261b6063c5ec591db35bb69d
Jaime Salgado <jsalg10@gmail.com>
2021-04-04 20:49:20 -0500

    updated mongodb

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ let db,
     dbConnectionStr = DB_String,
     dbName ="magic"
 
-MongoClient.connect()
+MongoClient.connect(DB_String, {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
