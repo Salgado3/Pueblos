b6333ba2b6c00fd0b815fa2de1ce8e56572e0775
Jaime Salgado <jsalg10@gmail.com>
2021-04-05 13:32:24 -0500

    updated to server. linked mongo

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const { MongoClient } = require("mongodb")
-
-
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+require('dotenv').config()
 
 
 //connect to Database
 let db,
-    dbConnectionStr = DB_String,
+    dbConnectionStr = process.env.DB_STRING,
     dbName ="magic"
 
-MongoClient.connect(DB_String, {useUnifiedTopology: true})
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
