de85d583209b2b00410f0cb0ab5b3cce808048af
Jaime Salgado <jsalg10@gmail.com>
2021-04-05 13:12:47 -0500

    setting up server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const { MongoClient } = require("mongodb")
 
+
+
+
+//connect to Database
 let db,
     dbConnectionStr = DB_String,
     dbName ="magic"
@@ -9,3 +13,14 @@ MongoClient.connect(DB_String, {useUnifiedTopology: true})
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
+
+//Setting up Server
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
+ 
