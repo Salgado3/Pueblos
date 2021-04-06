cfc87e08ad49e26aa1d9cb4897174b3af7171b54
Jaime Salgado <jsalg10@gmail.com>
2021-04-06 12:06:21 -0500

    updated server, ejs, and connected database. currently working

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 8000
+const PORT = 2121
 require('dotenv').config()
 
 
 //connect to Database
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName ="magic"
+    dbName ="pueblosMagicos"
 
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
+    
 
 //Setting up Server
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+// app.get('/',async (request, response)=>{
+//     const todoItems = await db.collection('pueblosMagicos').find().toArray()
+//     const itemsLeft = await db.collection('pueblosMagicos').countDocuments({completed: false})
+//     response.render('index.ejs', { items: todoItems, left: itemsLeft })
 
 app.listen(process.env.PORT || PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
