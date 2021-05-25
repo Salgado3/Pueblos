c9395e13b8e200396c011bbc692b04f1fcaf42f0
Jaime Salgado <jsalg10@gmail.com>
2021-05-25 08:42:28 -0500

    created client side js file to sync up button on feed ejs. work in progress

---
+++ public/clientJS/client.js
@@ -0,0 +1,36 @@
+
+const airportBtn = document.querySelectorAll(".airbtn").addEventListener('click',searchAirId)
+
+
+
+async function searchAirId() {
+    try{
+        console.log("the button is working")
+    }catch (err) {
+              console.log(err);
+            }
+}
+
+
+
+// async function searchAirId() {
+//     const sName = this.parentNode.querySelector(".stageName").innerText;
+//     const bName = this.parentNode.querySelector(".birthName").innerText;
+//     const tLikes = Number(this.parentNode.querySelector(".likes").innerText);
+//     try {
+//       const response = await fetch("/feed", {
+//         method: "put",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           stageNameS: sName,
+//           birthNameS: bName,
+//           likesS: tLikes,
+//         }),
+//       });
+//       const data = await response.json();
+//       console.log(data);
+//       location.reload();
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
