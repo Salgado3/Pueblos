5c9276737b0237b81f48664fa3adacf44b3535f6
Jaime Salgado <jsalg10@gmail.com>
2021-05-28 09:22:29 -0500

    worked on CSS for pueblo post page

--- controllers/api.js
+++ controllers/api.js
@@ -16,19 +16,4 @@ module.exports = {
       console.log(err);
     }
   },
-
-  //   airportCodes: async (req, res) => {
-  // //     //parce out the airport image URL and Title, then res.json() send back data in a format that is good to work with.
-  // //     try {
-  // //       const postsById = await Post.find(req.query.airportId);
-
-  // //       res.render("feed.ejs", {
-  // //         postsById: postsById,
-  // //         image__img: req.airportId,
-  // //       });
-  // //       console.log(data);
-  // //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   },
 };
