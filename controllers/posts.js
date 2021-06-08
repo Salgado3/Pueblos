ac29b9031a256029693aabc5c3cdae38cd9779af
Jaime Salgado <jsalg10@gmail.com>
2021-06-07 22:23:36 -0500

    updated delete button. Delete now removes the pueblo's from login users' profile page. (rm from array)

--- controllers/posts.js
+++ controllers/posts.js
@@ -67,16 +67,21 @@ module.exports = {
   },
   deletePost: async (req, res) => {
     try {
-      // Find post by id
-      let post = await Post.findById({ _id: req.params.id });
-      // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
-      // Delete post from db
-      await Post.remove({ _id: req.params.id });
-      console.log("Deleted Post");
+      await User.findOneAndUpdate(
+        { _id: req.user.id },
+
+        { $pull: { bookmark: req.params.id } }
+      );
       res.redirect("/profile");
     } catch (err) {
       res.redirect("/profile");
     }
   },
 };
+
+// // Find post by id
+// let post = await Post.findById({ _id: req.params.id });
+// // Delete image from cloudinary
+// await cloudinary.uploader.destroy(post.cloudinaryId);
+// // Delete post from db
+// await Post.remove({ _id: req.params.id });
