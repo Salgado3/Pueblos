5f5844082de2220b0d670cc508aeeac4a6a0af5d
Jaime Salgado <jsalg10@gmail.com>
2021-07-25 15:03:42 -0500

    working on V2, adding HERE map for Pueblos. Map still not rendering in Mx

---
+++ public/map/js/config.js
@@ -0,0 +1,11 @@
+const center = {
+  lat: 23.6345,
+  lng: 102.5528,
+  text: "Mexico City, Mexico",
+};
+
+const hereCredentials = {
+  apikey: "{HERE_API_KEY}",
+};
+
+export { center, hereCredentials };
