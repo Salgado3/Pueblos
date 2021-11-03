988d7d77fafd26da8113f2abe0229fb7655312c3
Jaime Salgado <jsalg10@gmail.com>
2021-11-03 10:44:05 -0500

    cleaned up routes/api. removed unused middleware

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/multer");
 const apiController = require("../controllers/api");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
 
 router.post("/getAirport", apiController.getAirport);
 //map
