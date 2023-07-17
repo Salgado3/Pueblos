aa8379bdc49886947b62398cf4b034f5d69b768f
Salgado3 <jsalg10@gmail.com>
2023-07-17 13:11:21 -0500

    updated log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,14 +46,13 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout();
- 
   req.session.destroy((err) => {
-    if (err)
+    if (err){
       console.log("Error : Failed to destroy the session during logout.", err);
+  }
     req.user = null;
     res.redirect("/");
   });
-  res.redirect("/");
 };
 
 exports.getSignup = (req, res) => {
