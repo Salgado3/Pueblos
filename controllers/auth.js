c3c484a38bdad638f0308723bf1c692e17a22407
Salgado3 <jsalg10@gmail.com>
2023-07-17 13:11:41 -0500

    updated logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ exports.logout = (req, res) => {
   req.logout();
   req.session.destroy((err) => {
     if (err){
-      console.log("Error : Failed to destroy the session during logout.", err);
+    console.log("Error : Failed to destroy the session during logout.", err);
   }
     req.user = null;
     res.redirect("/");
