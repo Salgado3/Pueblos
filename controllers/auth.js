fe0f55a898d851ec8176e7e0fa2308f6cd06a022
Salgado3 <jsalg10@gmail.com>
2023-07-17 13:03:19 -0500

    added redirect on logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,12 +46,14 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout();
+ 
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
     req.user = null;
     res.redirect("/");
   });
+  res.redirect("/");
 };
 
 exports.getSignup = (req, res) => {
